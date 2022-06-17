6966779cd0a4a40a10ce22b8eb08e29a148fd21b
Garren <39251346@qq.com>
2022-06-17 21:36:58 +0800

    11

--- Assets/Math/01-MatrixToEuler/MatrixToEuler.cs
+++ Assets/Math/01-MatrixToEuler/MatrixToEuler.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 namespace GARREN.URPEx
 {
     public class MatrixToEuler : MonoBehaviour
     {
+        #region Private Field
         [SerializeField]
         private Matrix4x4 m_RotateMatrix;
 
@@ -13,9 +11,60 @@ namespace GARREN.URPEx
         /// heading pitch bank 
         /// </summary>
         [SerializeField]
+        [ReadOnly]
         private float h, p, b;
 
-        
+        [SerializeField]
+        private Transform m_RotateRoot;
+        #endregion
+
+        #region Unity Loop
+
+        private void Update()
+        {
+            MatrixConvertEulerAngle();
+            SetTransformRootRotation();
+        }
+        #endregion
+
+        #region Rotate Action
+        private void MatrixConvertEulerAngle()
+        {
+            float sp = -m_RotateMatrix.m23;
+            if(sp<=-1.0f)
+            {
+                p = -Mathf.PI / 2;
+            }else if(sp>=1.0f)
+            {
+                p = Mathf.PI / 2;
+            }
+            else
+            {
+                p = Mathf.Asin(sp);
+            }
+
+            if(Mathf.Abs(sp)>0.9999f)
+            {
+                b = 0.0f;
+                h = Mathf.Atan2(-m_RotateMatrix.m31, m_RotateMatrix.m11);
+            }
+            else
+            {
+                h = Mathf.Atan2(m_RotateMatrix.m13, m_RotateMatrix.m33);
+
+                b = Mathf.Atan2(m_RotateMatrix.m21, m_RotateMatrix.m22);
+            }
+        }
+
+        private void SetTransformRootRotation()
+        {
+            if(m_RotateRoot)
+            {
+                m_RotateRoot.eulerAngles = new Vector3(p, h, b);
+            }
+        }
+        #endregion
+
     }
 
 }
